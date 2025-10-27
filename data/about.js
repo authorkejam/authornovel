6325b65c97a64611da7ce0be86ae66573a56b7f6
authorkejam <authornovel527@gmail.com>
2025-10-27 09:42:02 +0800

    Add files via upload

---
+++ data/about.js
@@ -0,0 +1,17 @@
+let about = [
+  {
+    id: 0,
+    title: "About",
+    date: "01-01-2023",
+    chapters: [
+      {
+        title: "About the Author",
+        file: "About/about.md"
+      },
+      {
+        title: "Contact",
+        file: null // No file, render form
+      }
+    ]
+  }
+];
---
+++ data/blog.js
@@ -0,0 +1,57 @@
+let blogs = [
+  {
+    id: 0,
+    title: "Sample Blog",
+    date: "01-01-2023",
+    chapters: [
+      {
+        title: "Update Animasi",
+        file: "Blog/BlogUpdate/5sampleblog.md"
+      },
+      {
+         title: "Short Story",
+        file: "Blog/BlogInformasi/blog7.md"
+      },
+      {
+        title: "Responsivee web",
+        file: "Blog/BlogInformasi/blog6.md"
+      },
+      {
+        title: "Sistem Memory (Oct 11, 2025)",
+        file: "Blog/BlogInformasi/blog5.md"
+      },
+      {
+        title: "Perbaikan Novel/sistem no.1 (Oct 11, 2025)",
+        file: "Blog/BlogInformasi/blog4.md"
+      },
+      {
+        title: "Short Story",
+        file: "Blog/BlogInformasi/blog3.md"
+      },
+      {
+        title: "Update Vers3 (Oct 03, 2025)",
+        file: "Blog/BlogUpdate/3sampleblog.md"
+      },
+      {
+        title:"Sistem Temani Aku (Oct 03, 2025)",
+        file: "Blog/BlogUpdate/4sampleblog.md"
+      },
+      {
+        title: "Novel sampel (Apr 01, 2025)",
+        file: "Blog/BlogInformasi/blog2.md"
+      },
+      {
+        title: "Update Ver 2",
+        file: "Blog/BlogUpdate/2sampleblog.md"
+      },
+      {
+        title: "Novel sampel (Jan 10, 2025)",
+        file: "Blog/BlogInformasi/blog1.md"
+      },
+      {
+        title: "Update Vers 1",
+        file: "Blog/BlogUpdate/1sampleblog.md"
+      },
+    ]
+  }
+];
